72a606a18254786028718d09475371db2dbdaf45
Back2RL <leonardoertelt@gmail.de>
2016-05-29 16:29:04 +0200

    implemented an interface to identify targets and to get their information

---
+++ AR_rev_v_curr/Private/Target_Interface.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AR_rev_v_curr.h"
+#include "Target_Interface.h"
+
+UTarget_Interface::UTarget_Interface(const FObjectInitializer &ObjectInitializer) : Super(ObjectInitializer) 
+{
+}
+
---
+++ AR_rev_v_curr/Public/Target_Interface.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "Target_Interface.generated.h"
+
+/**
+ * 
+ */
+
+UINTERFACE(Blueprintable, MinimalAPI)
+class UTarget_Interface : public UInterface
+{
+	GENERATED_UINTERFACE_BODY()
+};
+
+class ITarget_Interface
+{
+	GENERATED_IINTERFACE_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Target")
+		bool GetIsTargetable();
+
+
+
+};
