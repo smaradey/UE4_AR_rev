65ce9ef610b85e6cfdef9f5a6a76af35726279a9
Back2RL <leonardoertelt@gmail.de>
2016-10-03 20:27:16 +0200

    interface to get messages form the gunfire component

---
+++ AR_rev_v_curr/Private/Gun_Interface.cpp
@@ -0,0 +1,22 @@
+//// Fill out your copyright notice in the Description page of Project Settings.
+//
+//#include "AR_rev_v_curr.h"
+//#include "Gun_Interface.h"
+//
+//Gun_Interface::Gun_Interface()
+//{
+//}
+//
+//Gun_Interface::~Gun_Interface()
+//{
+//}
+
+
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AR_rev_v_curr.h"
+#include "Gun_Interface.h"
+
+UGun_Interface::UGun_Interface(const FObjectInitializer &ObjectInitializer) : Super(ObjectInitializer)
+{
+}
---
+++ AR_rev_v_curr/Public/Gun_Interface.h
@@ -0,0 +1,46 @@
+//// Fill out your copyright notice in the Description page of Project Settings.
+//
+//#pragma once
+//
+///**
+// * 
+// */
+//class AR_REV_V_CURR_API Gun_Interface
+//{
+//public:
+//	Gun_Interface();
+//	~Gun_Interface();
+//};
+//--------------------
+
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "Projectile.h"
+#include "Gun_Interface.generated.h"
+
+/**
+*
+*/
+
+UINTERFACE(Blueprintable, MinimalAPI)
+class UGun_Interface : public UInterface
+{
+	GENERATED_UINTERFACE_BODY()
+};
+
+class IGun_Interface
+{
+	GENERATED_IINTERFACE_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Gun")
+		bool GetIsTargetable();
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Gun")
+		void ProjectileSpawned(const FProjectileProperties& Properties, const FTransform& SpawnTransform);
+
+
+
+};
