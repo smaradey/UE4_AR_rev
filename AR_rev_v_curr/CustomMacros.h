0b5ca6a2c22e6e0790a1c1c6bda0cd1c3da6269f
Back2RL <leonardoertelt@gmail.de>
2016-11-02 14:18:40 +0100

    added LOG Macros for 1/2 additional Parameters

--- AR_rev_v_curr/CustomMacros.h
+++ AR_rev_v_curr/CustomMacros.h
@@ -4,6 +4,10 @@
 #endif
 #if LOG_MSG == 1
 #define LOG(message) UE_LOG(LogTemp, Log, TEXT(message));
+#define LOGA(message,arg) UE_LOG(LogTemp, Log, TEXT(message),arg);
+#define LOGA2(message,arg1,arg2) UE_LOG(LogTemp, Log, TEXT(message),arg1,arg2);
 #else
 #define LOG(message)
+#define LOGA(message,arg)
+#define LOGA2(message,arg1,arg2)
 #endif
