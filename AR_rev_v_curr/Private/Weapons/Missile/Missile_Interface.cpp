1e90c2b930a48374d999b3d9e1f6fdaf1f9c9370
Back2RL <leonardoertelt@gmail.de>
2016-11-02 14:20:28 +0100

    create a missile Interface

---
+++ AR_rev_v_curr/Private/Weapons/Missile/Missile_Interface.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AR_rev_v_curr.h"
+#include "Missile_Interface.h"
+
+
+// This function does not need to be modified.
+UMissile_Interface::UMissile_Interface(const class FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+// Add default functionality here for any IMissile_Interface functions that are not pure virtual.
---
+++ AR_rev_v_curr/Public/Weapons/Missile/Missile_Interface.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "Missile_Interface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(Blueprintable, MinimalAPI)
+class UMissile_Interface : public UInterface
+{
+	GENERATED_UINTERFACE_BODY()
+};
+
+/**
+ * 
+ */
+class AR_REV_V_CURR_API IMissile_Interface
+{
+	GENERATED_IINTERFACE_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Missile_Interface")
+		void Explode();
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Missile_Interface")
+		void DeactivateForDuration(const float Duration);
+};
