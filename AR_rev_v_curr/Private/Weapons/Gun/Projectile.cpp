640f7f6e3706774f0f3bc241e13ddbd5dc47b325
Back2RL <leonardoertelt@gmail.de>
2016-11-15 15:35:57 +0100

    implemented custom linetrace projectiles

--- AR_rev_v_curr/Private/Weapons/Gun/Projectile.cpp
+++ AR_rev_v_curr/Private/Weapons/Gun/Projectile.cpp
@@ -3,26 +3,176 @@
 #include "AR_rev_v_curr.h"
 #include "Projectile.h"
 
+#define DEBUG 0
+
 
 // Sets default values
-AProjectile::AProjectile()
+AProjectile::AProjectile(const FObjectInitializer& PCIP) : Super(PCIP)
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// public
+	MinVelocitySquarred = 2500.0f;
+	Bounciness = 0.25f;
+	MaxBounces = 2;
+	BounceThreshold = -0.8f;
+
+	// private
+	Bounces = 0;
 }
 
 // Called when the game starts or when spawned
 void AProjectile::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	Velocity = GetActorForwardVector() * ProjectileProperties.MuzzleVelocity + AdditionalVelocity;
+
+	bUnlimitedBouncing = MaxBounces < 0;
+
+	const float newLifeSpan = ProjectileProperties.MaxRange / FMath::Max(1.0f, ProjectileProperties.MuzzleVelocity);
+	SetLifeSpan(newLifeSpan);
+	LOGA("Projectile: BeginPlay: Set Lifespan to %f", newLifeSpan);
+
+	// update the rotation to point in the direction of the velocity
+	SetActorRotation(Velocity.Rotation(), ETeleportType::TeleportPhysics);
+
+	// TODO: recalc Tracer Length
+	// TODO: random forward movement for realism
+
+	OnBounce();
 }
 
 // Called every frame
-void AProjectile::Tick( float DeltaTime )
+void AProjectile::Tick(float DeltaTime)
 {
-	Super::Tick( DeltaTime );
-
+	Super::Tick(DeltaTime);
+	Movement();
 }
 
+void AProjectile::OnBounce_Implementation()
+{
+}
+
+void AProjectile::OnImpact_Implementation()
+{
+}
+
+void AProjectile::Movement()
+{
+	TraceStartLocation = GetActorLocation();
+	PendingTravel = 1.0f;
+	do
+	{
+		bBounceAgain = false;
+		TraceAfterBounce();
+	} while (bBounceAgain);
+}
+
+void AProjectile::TraceAfterBounce()
+{
+	const UWorld* const World = GetWorld();
+	if (World)
+	{
+		switch (ProjectileProperties.ProjectileType)
+		{
+		case EProjectileType::Kinetic:
+		{
+			FVector Gravity = World ? FVector(0, 0, World->GetGravityZ()) : FVector::ZeroVector;
+			UCalcFunctionLibrary::GetWorldGravity(this, Gravity);
+			Velocity = Velocity + Gravity * (PendingTravel * World->DeltaTimeSeconds);
+		}
+		break;
+		case EProjectileType::Energy: break;
+		default: break;
+		}
+		TraceEndLocation = TraceStartLocation + Velocity * (World->DeltaTimeSeconds * PendingTravel);
+
+		FCollisionObjectQueryParams TraceParams;
+		TraceParams.AllObjects;
+		TraceParams.RemoveObjectTypesToQuery(ECC_Projectile);
+		FHitResult HitResult;
+#if DEBUG == 1
+		const float DEBUGLineLifeTime = 5.0f;
+#endif
+		if (World->LineTraceSingleByObjectType(HitResult, TraceStartLocation, TraceEndLocation, TraceParams))
+		{
+#if DEBUG == 1
+			// Red up to the blocking hit, green thereafter
+			::DrawDebugLine(World, TraceStartLocation, HitResult.ImpactPoint, FColor::Red, false, DEBUGLineLifeTime);
+			::DrawDebugLine(World, HitResult.ImpactPoint, TraceEndLocation, FColor::Green, false, DEBUGLineLifeTime);
+			::DrawDebugPoint(World, HitResult.ImpactPoint, 16.f, FColor::Red, false, DEBUGLineLifeTime);
+#endif
+			HandleTraceResult(HitResult);
+		}
+		else
+		{
+#if DEBUG == 1
+			// no hit means all red
+			::DrawDebugLine(World, TraceStartLocation, TraceEndLocation, FColor::Red, false, DEBUGLineLifeTime);
+#endif
+			UpdateTransform();
+		}
+	}
+}
+
+bool AProjectile::BouncingAllowed()
+{
+	return bUnlimitedBouncing ^ (Bounces < MaxBounces);
+}
+
+bool AProjectile::CanBounce(const FHitResult& Hit)
+{
+	return FVector::DotProduct(Hit.Normal, Velocity.GetSafeNormal()) > BounceThreshold;
+}
+
+void AProjectile::Impact(const FHitResult& Hit)
+{
+	OnImpact();
+	LOG("Projectile: Impact")
+		Destroy();
+}
+
+void AProjectile::Bounce(const FHitResult& Hit)
+{
+	OnBounce();
+	PendingTravel *= (1.0f - Hit.Time);
+	TraceStartLocation = Hit.ImpactPoint; // + Hit.Normal;
+	Velocity = Velocity.MirrorByVector(Hit.Normal) * Bounciness;
+	++Bounces;
+	if (Velocity.SizeSquared() < MinVelocitySquarred)
+	{
+		Destroy();
+	}
+	else
+	{
+		// TODO: Recalc Tracer Length
+		bBounceAgain = true;
+	}
+}
+
+void AProjectile::HandleTraceResult(const FHitResult& Hit)
+{
+	if (BouncingAllowed())
+	{
+		if (CanBounce(Hit))
+		{
+			Bounce(Hit);
+		}
+		else
+		{
+			Impact(Hit);
+		}
+	}
+	else
+	{
+		Impact(Hit);
+	}
+}
+
+void AProjectile::UpdateTransform()
+{
+	const FRotator newRotation = (TraceEndLocation - TraceStartLocation).Rotation();
+	SetActorLocationAndRotation(TraceEndLocation, newRotation, false, nullptr, ETeleportType::TeleportPhysics);
+}
--- AR_rev_v_curr/Public/Weapons/Gun/Projectile.h
+++ AR_rev_v_curr/Public/Weapons/Gun/Projectile.h
@@ -6,17 +6,23 @@
 #include "Damage.h"
 #include "Projectile_Enums.h"
 #include "Projectile_Structs.h"
+#define LOG_MSG 1
+#include "CustomMacros.h"
+#include "CalcFunctionLibrary.h"
 #include "Projectile.generated.h"
+
+// custom collision profile created in Editor, Tracechannel found in DefaultEngine.ini
+#define ECC_Projectile ECC_GameTraceChannel1
 
 
 UCLASS()
+
 class AR_REV_V_CURR_API AProjectile : public AActor
 {
 	GENERATED_BODY()
-
 public:
 	// Sets default values for this actor's properties
-	AProjectile();
+	AProjectile(const FObjectInitializer& ObjectInitializer);
 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -24,15 +30,67 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaSeconds) override;
 
+
 	// struct that holds all important properties that defines the projectiles behaviour
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (ExposeOnSpawn = true), Category = "Projectile|Settings")
 		FProjectileProperties ProjectileProperties;
 
-	void SetProjectileProperties(const FProjectileProperties& Properties)
-	{
-		ProjectileProperties = Properties;
-	}
+	// Velocity that is being inherited from the weapon that is shooting the projectile (in world Space)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (ExposeOnSpawn = true), Category = "Projectile|Movement")
+		FVector AdditionalVelocity;
+
+	// Current Velocity vector of the Projectile in world space
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile|Movement")
+		FVector Velocity;
+
+	// if the velocity of the projectile is lower it will be destroyed, set to 0 to disable the destruction
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile|Movement")
+		float MinVelocitySquarred;
+
+	// actors to ignore while moving
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (ExposeOnSpawn = true), Category = "Projectile|Collision")
+		TArray<AActor*> IgnoreActors;
+
+	// 1.0 -> 100% Velocity after bounce
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile|Movement")
+		float Bounciness;
+
+	// -1 unlimited bounces; 0 bouncing disabled
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile|Movement")
+		int32 MaxBounces;
+
+	// dot product of the velocity vector and the surface normal, choose in range of -1.0 (always bounce) and 0.0 (never bounce)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile|Movement")
+		float BounceThreshold;
 
 
 
+
+private:
+	FVector TraceStartLocation;
+	FVector TraceEndLocation;
+	float PendingTravel;
+	FVector TempVelocity;
+	int32 Bounces;
+	bool bUnlimitedBouncing;
+	bool bBounceAgain;
+
+protected:
+	UFUNCTION(BlueprintNativeEvent, Category = "Projectile|Collsion")
+		void OnBounce();
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Projectile|Collsion")
+		void OnImpact();
+
+	virtual void Movement();
+
+	virtual void TraceAfterBounce();
+
+	virtual FORCEINLINE bool BouncingAllowed();
+	virtual FORCEINLINE bool CanBounce(const FHitResult& Hit);
+
+	virtual void Impact(const FHitResult& Hit);
+	virtual void Bounce(const FHitResult& Hit);
+	virtual void HandleTraceResult(const FHitResult& Hit);
+	virtual void UpdateTransform();
 };
